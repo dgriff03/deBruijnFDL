15525f52f82a20e26892481d6cb8f4b4ecb7ddba
dgriff03 <daniel.griffin@tufts.edu>
2015-04-18 00:30:21 -0400

    added init forces

--- FDL.py
+++ FDL.py
@@ -1,6 +1,8 @@
 from Tkinter import Scale,Tk,Canvas,HORIZONTAL,FLAT,LEFT,RIGHT,CENTER
-import random
 from constants import width, height
+
+SPRINT_CONSTANT = 0.001
+ELECT_CONSTANT = 0.1
 
 
 root = Tk()
@@ -27,9 +29,12 @@ class fdlNode(object):
         self.name = name
         self.linked = [] #List of FDLNode -- direction agnostic
         self.branch = False #Used for de Bruijn Graph methods
+        self.__fx = 0
+        self.__fy = 0
+        self.__t = 1
+        self.__decay = 0.9
 
-
-    def addCanvas(self,canv, x, y,t, mass, color):
+    def addCanvas(self,canv, x, y, mass, color):
         self.x, self.y = x, y
         self._canvas = canv
         self.mass = mass
@@ -37,7 +42,6 @@ class fdlNode(object):
         self._index = canv.create_oval(coord, fill=color)
         self._vx = 0
         self._vy = 0
-        self._t = t
 
     
     def __repr__(self):
@@ -46,8 +50,38 @@ class fdlNode(object):
             s += n.name + "\n"
         return s
 
+    def addForce(self,node):
+        #called on each node before motion
+        #ADD ELECTRICAL FORCE
+        dx = (node.x - self.x) if (node.x - self.x) != 0 else 0.01
+        dy = (node.y - self.y) if (node.y - self.y) != 0 else 0.01
+        dfx = (ELECT_CONSTANT * node.mass * self.mass ) / dx
+        dfy = (ELECT_CONSTANT * node.mass * self.mass ) / dy
+
+        self.__fx += dfx
+        self.__fy += dfy
+
+        if node in self.linked:
+            self.__fx += SPRINT_CONSTANT * (node.x - self.x)
+            self.__fy += SPRINT_CONSTANT * (node.y - self.y)
+
+        
+    def update(self):
+        self.__decay = decayInput.get() / 100.0
+        self.__t = timeInput.get() / 10.0
+        self.__fx *= self.__decay
+        self.__fy *= self.__decay
+        ax = self.__fx / self.mass
+        ay = self.__fy / self.mass
+        self._vx += ax * self.__t
+        self._vy += ay * self.__t
+        self._vx *= self.__decay
+        self._vy *= self.__decay
+        self.__fx = 0
+        self.__fy = 0
 
     def move(self):
+        self.update()
         x1, y1, x2, y2 = self._canvas.coords(self._index)
         root_w = self._canvas.winfo_width()
         root_h = self._canvas.winfo_height()
@@ -67,8 +101,9 @@ class fdlNode(object):
                 dy = root_h - y2 
             self._canvas.move(self._index, 0, dy)
             self._vy *= -1
-        dx = self._vx * self._t
-        dy = self._vy * self._t
+        dx = self._vx * self.__t
+        dy = self._vy * self.__t
+        print self._vy
 
         self._canvas.move(self._index, dx, dy)
         tempX1, tempY1, tempX2, tempY2 = self._canvas.coords(self._index)
@@ -78,14 +113,19 @@ class fdlNode(object):
 
 
 def move_nodes(nodes, lines, root):
-    for c in nodes:
-        c.move()
+    for ni in nodes:
+        for nj in nodes:
+            if ni != nj:
+                ni.addForce(nj)
+    for n in nodes:
+        n.update()
+        n.move()
     for line in lines:
         lid = line[0]
         n1 = line[1]
         n2 = line[2]
         canvas.coords( lid, n1.x, n1.y, n2.x, n2.y  )
-    root.after(timeInput.get() , lambda: move_nodes(nodes, lines, root))
+    root.after(1 , lambda: move_nodes(nodes, lines, root))
 
 
 def getCanvas():
